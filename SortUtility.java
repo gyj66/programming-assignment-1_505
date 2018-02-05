dd694c76a3b50c2634901f3e95954bccf35f46d1
gyj66 <guiyujie9391@hotmail.com>
2018-02-04 19:21:33 -0500

    Add files via upload

---
+++ SortUtility.java
@@ -0,0 +1,50 @@
+class SortUtility
+{
+       
+
+
+
+public static void main(String[] args)
+throws IOException {
+
+
+BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	 String line;
+	 line=in.readLine();
+    
+    ArrayList<Integer> ar=new ArrayList<>();
+	
+	while((line=in.readLine())!=null)
+	{
+       ar.add(Integer.parseInt(line));
+       
+
+	}
+
+long start=System.currentTimeMillis();
+     Integer comparnums=ar.sort(new Comparator<Integer>() {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        comparisons++;
+        return o1.compareTo(o2);
+    } 
+    });       
+long end=System.currentTimeMillis(); 
+long total=end-start;
+   
+	for(int i=0;i<ar.size();i++)
+	{
+
+		System.out.println(ar.get(i));
+	}
+
+ System.err.println("runtime"+","+total);
+ System.err.println("comparisons"+","+comparnums);
+
+}
+
+
+
+}
+
+
